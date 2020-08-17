2be5924fca05ccb355c2fb02c29254082f06e525
MrA87 <57851232+MrA87@users.noreply.github.com>
2020-08-17 13:42:42 +1000

    70 percent

--- Assets/LoadAssests.cs
+++ Assets/LoadAssests.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class LoadAssests : MonoBehaviour
 {
     public GameObject redObj;
-
+    [SerializeField] private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
-        
+             
+        Instantiate(redObj, Vector3.right * 2 , Quaternion.identity);
+        Instantiate(blueObj, Vector3.left * 2, Quaternion.identity);
+
     }
 
     // Update is called once per frame
